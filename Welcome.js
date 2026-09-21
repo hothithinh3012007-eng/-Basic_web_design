d9ea67db52bb74b3010db5c403ce7bd9a5cc6d2d
hothithinh3012007-eng <hothithinh3012007@gmail.com>
2026-09-21 16:49:21 +0700

    Delete Welcome.js

--- Welcome.js
+++
@@ -1,7 +0,0 @@
-//  alert(" Chào mừng đến với website ");
-console.log("Chaod mừng đến với website ");
-let sum = 0;
-let a = 10;
-let b = 20;
-sum = a + b;
-console.log("The sum of a and b is:" + sum)
